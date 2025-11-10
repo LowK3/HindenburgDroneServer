d6fd793c0879d944ae36da042043aea9bf439031
Rando Kübar <randokubargmail.com>
2025-11-10 21:22:22 +0200

    Fix: bigger delays for reconnect

--- Drone_Server/Network/tcp_video_server.py
+++ Drone_Server/Network/tcp_video_server.py
@@ -14,6 +14,7 @@ class TCPServer:
     def start(self):
         self.server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_LINGER, struct.pack('ii', 1, 0))
         self.server_sock.bind(("", TCP_PORT))
         self.server_sock.listen(1)
         self.server_sock.settimeout(TCP_ACCEPT_TIMEOUT)
