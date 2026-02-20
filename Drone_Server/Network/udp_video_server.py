236eab3fc61e6772915bc8962ca0ca56e96858de
Rando Kübar <randokubargmail.com>
2026-02-20 18:52:13 +0200

    RGB to BGR

--- Drone_Server/Network/udp_video_server.py
+++ Drone_Server/Network/udp_video_server.py
@@ -23,7 +23,7 @@ class VideoServer:
                     continue
 
                 try:
-                    data = simplejpeg.encode_jpeg(frame, quality=JPEG_QUALITY, colorspace='RGB')
+                    data = simplejpeg.encode_jpeg(frame, quality=JPEG_QUALITY, colorspace='BGR')
                 except Exception as e:
                     log(f"Encode failed: {e}")
                     continue
