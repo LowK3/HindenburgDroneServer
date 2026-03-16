21499a4cc1dbf41cf27a481b9a5931b473dc0343
Rando Kübar <randokubargmail.com>
2026-03-16 19:08:33 +0200

    Resolution test

--- Drone_Server/config.py
+++ Drone_Server/config.py
@@ -6,7 +6,7 @@ UDP_VIDEO_PORT = 8486
 CONTROL_TCP_PORT = 8600
 
 """ Camera settings """
-CAM_RESOLUTION = (1680, 1232)
+CAM_RESOLUTION = (1920, 1080)
 CAM_FPS = 30
 JPEG_QUALITY = 60
 
@@ -19,7 +19,7 @@ TCP_SEND_TIMEOUT = 0.5
 """ Logging """
 LOG_PREFIX = "[SERVER]"
 
-""" Motor control pins (ApisQueen U2 Mini-S) """
+""" Motor control pins (ApisQueen U2 Mini) """
 REAR_LEFT_PIN  = 13
 REAR_RIGHT_PIN = 21
 FRONT_LEFT_PIN = 5
