770df7c57b5c210371b9e79f26541e404cdc742e
Rando Kübar <randokubargmail.com>
2026-03-17 22:30:16 +0200

    Added logging to file so faults could be discovered after the dive

--- Drone_Server/Utils/common.py
+++ Drone_Server/Utils/common.py
@@ -1,10 +1,30 @@
-import sys, time, select
+import sys, time, select, logging, os
+from logging.handlers import RotatingFileHandler
 from config import LOG_PREFIX
+
+LOG_DIR = "logs"
+if not os.path.exists(LOG_DIR):
+    os.makedirs(LOG_DIR)
+
+LOG_FILE = os.path.join(LOG_DIR, "system.log")
+
+logger = logging.getLogger("ServerLogger")
+logger.setLevel(logging.INFO)
+
+file_handler = RotatingFileHandler(
+    LOG_FILE, 
+    maxBytes = 5 * 1024 * 1024, 
+    backupCount = 2
+)
+
+formatter = logging.Formatter("[SERVER] [%(asctime)s] | %(message)s", datefmt="%H:%M:%S")
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
 
 def log(msg: str):
     ts = time.strftime("%H:%M:%S")
     print(f"{LOG_PREFIX} {ts} | {msg}")
-    sys.stdout.flush()
+    logger.info(msg)
 
 def check_keyboard(shutdown_event):
     """
@@ -15,7 +35,7 @@ def check_keyboard(shutdown_event):
         dr, _, _ = select.select([sys.stdin], [], [], 0)
         if dr:
             key = sys.stdin.read(1)
-            if key.lower() == 'q':
+            if key.lower() == "q":
                 log("Shutdown key 'q' pressed")
                 shutdown_event.set()
     except Exception:
