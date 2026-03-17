cb4f619ad4f554468bc124259b481c90fec73996
Rando Kübar <randokubargmail.com>
2026-03-17 18:23:30 +0200

    Video Test

--- Drone_Server/config.py
+++ Drone_Server/config.py
@@ -7,8 +7,8 @@ CONTROL_TCP_PORT = 8600
 
 """ Camera settings """
 CAM_RESOLUTION = (1680, 1232)
-CAM_FPS = 40
-JPEG_QUALITY = 60
+CAM_FPS = 60
+JPEG_QUALITY = 50
 
 """ Connection timeouts """
 UDP_TIMEOUT = 0.25
